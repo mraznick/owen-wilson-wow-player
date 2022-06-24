d00a33e3310d9415674294cf46a842f56467f192
Mitchell Raznick <mraznick@Mitchells-MacBook-Air.local>
2022-06-24 12:03:06 -0400

    grabbed 50+ film images; need to figure out how to remove duplicates

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-let url = 'https://owen-wilson-wow-api.herokuapp.com/'
+let url = 'https://owen-wilson-wow-api.herokuapp.com/wows/ordered/1-50'
 
 fetch(url)
-  .then(res => res.json())
+  .then(response => response.json())
+  .then(response => {
+
+    for (let i = 0; i < response.length; i++) {
+      function createList() {
+        let posterDiv = document.createElement('div')
+        posterDiv.className = "poster-div"
+
+        let posterImg = document.createElement('img')
+        posterImg.className = "poster-image"
+        posterImg.src = `${response[i].poster}`
+
+        posterDiv.appendChild(posterImg)
+
+        document.querySelector('.parent-div').appendChild(posterDiv)
+      }
+      createList();
+    }
+  })
