a389b27f030ffefca12cc8372ecc70b63887abca
Mitchell Raznick <mraznick@mitchells-air.myfiosgateway.com>
2022-06-24 20:28:06 -0400

    fixed duplicate issue, started CSS layout

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
-let url = 'https://owen-wilson-wow-api.herokuapp.com/wows/ordered/1-50'
+let url = 'https://owen-wilson-wow-api.herokuapp.com/wows/ordered/1-85'
 
 fetch(url)
   .then(response => response.json())
   .then(response => {
-
     for (let i = 0; i < response.length; i++) {
+      const check = {}
+      let filteredRes = response.filter((res) => {
+        if (!check[res.movie]) {
+          check[res.movie] = 1
+          return check[res.movie]
+        }
+      })
       function createList() {
         let posterDiv = document.createElement('div')
         posterDiv.className = "poster-div"
 
         let posterImg = document.createElement('img')
         posterImg.className = "poster-image"
-        posterImg.src = `${response[i].poster}`
+        posterImg.src = `${filteredRes[i].poster}`
 
         posterDiv.appendChild(posterImg)
 
