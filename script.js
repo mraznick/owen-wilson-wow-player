ba56ec4fbf1b68b208b3268648db5ffdf4be26a2
Mitchell Raznick <mraznick@Mitchells-MacBook-Air.local>
2022-06-27 10:59:25 -0400

    started modals and event listeners

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-let url = 'https://owen-wilson-wow-api.herokuapp.com/wows/ordered/1-85'
+let url = 'https://owen-wilson-wow-api.herokuapp.com/wows/ordered/1-10'
+
+
+
 
 fetch(url)
   .then(response => response.json())
@@ -17,12 +20,48 @@ fetch(url)
 
         let posterImg = document.createElement('img')
         posterImg.className = "poster-image"
+        posterImg.id = "modal-target"
         posterImg.src = `${filteredRes[i].poster}`
+        posterImg.setAttribute('data-modal-target', '#modal')
 
         posterDiv.appendChild(posterImg)
 
         document.querySelector('.parent-div').appendChild(posterDiv)
+
+        openModalButtons = document.querySelectorAll('[data-modal-target]')
+        let closeModalButtons = document.querySelectorAll('[data-close]')
+        let overlay = document.getElementById('overlay')
+        openModalButtons.forEach(button => {
+          button.addEventListener('click', () => {
+            const modal = document.querySelector(button.dataset.modalTarget)
+            console.log('hello')
+            openModal(modal)
+
+            closeModalButtons.forEach(button => {
+              button.addEventListener('click', () => {
+                const modal = button.closest('.modal')
+                closeModal(modal)
+              })
+            })
+          })
+        })
       }
       createList();
     }
   })
+
+
+let openModalButtons
+
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
